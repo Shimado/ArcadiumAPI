69a07af8960359f621414dfaae39693eebb73504
BuildTools <unconfigured@null.spigotmc.org>
2026-06-18 19:27:12 +0300

    Changes

--- src/main/java/com/github/Shimado/arcadiumapi/enums/TableAnimationType.java
+++ src/main/java/com/github/Shimado/arcadiumapi/enums/TableAnimationType.java
@@ -2,8 +2,12 @@ package com.github.Shimado.arcadiumapi.enums;
 
 public enum TableAnimationType {
 
-    CIRCLE_PURPLE,
-    CIRCLE_FIRE,
-    CIRCLE_GREEN
+    CIRCLE_RED,
+    CIRCLE_ORANGE,
+    CIRCLE_YELLOW,
+    CIRCLE_BLUE,
+    CIRCLE_CYAN,
+    CIRCLE_LIME,
+    CIRCLE_MAGENTA
 
 }
